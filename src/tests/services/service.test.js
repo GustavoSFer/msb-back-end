87848ea07f8d55a1a02903f774145cddfe2dc613
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-07-23 20:07:23 -0300

    Test: create

--- src/tests/services/service.test.js
+++ src/tests/services/service.test.js
@@ -6,7 +6,7 @@ const { bodycreate, dataDb } = require('../Mock');
 
 
 describe('#Service', () => {
-  describe('#find', function () {
+  describe('#Find', function () {
     it('Deve retornar uma lista quando tem dados no banco', async () => {
       Sinon.stub(model, 'getAll').resolves(dataDb);
       const data = await service.getAll();
@@ -17,6 +17,14 @@ describe('#Service', () => {
       Sinon.stub(model, 'getAll').resolves([]);
       const data = await service.getAll();
       expect(data).to.be.empty;
+    });
+  });
+  describe('#Create', function () {
+    it('Enviando dados para o banco quando esta correto', async () => {
+      Sinon.stub(model, 'createForm').resolves(dataDb[0]);
+      const data = await service.createForm(bodycreate);
+      expect(data).to.deep.eq(dataDb[0]);
+      Sinon.restore();
     });
   });
 });
